4f585c0d78abcd61aa51ae12440207ebae8e068a
Dileep K <dlpkmr706@gmail.com>
2024-03-17 15:01:22 +0530

    created component to display no data

---
+++ src/components/no-data/NotFound.tsx
@@ -0,0 +1,15 @@
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        height: 100,
+        width: "40vw",
+      }}
+      className="flex justify-center align-center"
+    >
+      <h4 style={{ color: "orangered" }}>no data found</h4>
+    </div>
+  );
+};
+
+export default NotFound;
